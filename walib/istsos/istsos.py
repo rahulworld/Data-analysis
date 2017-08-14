2b7b36ea0efe0d22b02c95b19e33826982bf545a
rahulworld <chauhanr371@gmail.com>
2017-08-14 23:57:49 +0530

    addedhydroevents

--- walib/istsos/istsos.py
+++ walib/istsos/istsos.py
@@ -1662,23 +1662,26 @@ class HydroEventsTh(waIstsos):
 
         HyE=HydroEvents12(rise_lag=rise, fall_lag=fall, window=window,min_peak=peak, suffix=suffix, period=period)
         resdata=HyE.execute12(df)
-
-        values = np.array(resdata[0]['data'])
-        times = resdata[0].index
-        times_string =[]
-        for i in times:
-            times_string.append(str(i))
-
-        def convert_to_timestamp(a):
-            dt = datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
-            return int(time.mktime(dt.timetuple()))
-
-        times_timestamp = map(convert_to_timestamp, times_string)
-
+        dataFull=[]
         data4 = []
-        for i in range(len(times_string)):
-            a = [times_timestamp[i], values[i]]
-            data4.append(a)
+
+        for i in range(len(resdata)):
+            values = np.array(resdata[i]['data'])
+            times = resdata[i].index
+            times_string =[]
+            for i in times:
+                times_string.append(str(i))
+
+            def convert_to_timestamp(a):
+                dt = datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
+                return int(time.mktime(dt.timetuple()))
+
+            times_timestamp = map(convert_to_timestamp, times_string)
+
+            for i in range(len(times_string)):
+                a = [times_timestamp[i], values[i]]
+                data4.append(a)
+            # dataFull.append(data4)
 
         self.setData(data4)
         self.setMessage("Hydro events is successfully working")
